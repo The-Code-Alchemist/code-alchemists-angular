6c6f7c5df3926602e9213948a266674c55c8b850
The Code Alchemist <onur.akgun@code-alchemists.com>
2025-05-05 23:05:20 +0200

    Added URLs to Conferences, redundant import removed

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
-import {NgOptimizedImage} from '@angular/common';
 
 @Component({
   selector: 'app-about',
   imports: [
-    NgOptimizedImage
   ],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
